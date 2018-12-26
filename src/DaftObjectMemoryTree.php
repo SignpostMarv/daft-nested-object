04ff6155b204ea45f7512bfaaa26ecc356eefb6c
SignpostMarv <me@signpostmarv.name>
2018-12-26 14:18:18 +0000

    being rather more specific with the type hinting

--- src/DaftObjectMemoryTree.php
+++ src/DaftObjectMemoryTree.php
@@ -13,7 +13,7 @@ abstract class DaftObjectMemoryTree extends DaftObjectMemoryRepository implement
     public function RecallDaftNestedObjectFullTree(int $relativeDepthLimit = null) : array
     {
         /**
-        * @var DaftNestedObject[]
+        * @var array<int, DefinesOwnUntypedIdInterface&DaftNestedObject|DefinesOwnStringIdInterface&DaftNestedObject|DefinesOwnIntegerIdInterface&DaftNestedObject|DefinesOwnArrayIdInterface&DaftNestedObject>
         */
         $out = $this->memory;
 
